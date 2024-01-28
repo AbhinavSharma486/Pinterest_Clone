707b4ee4583bf273556656c5486d95e1c6aad0ab
AbhinavSharma486 <abhinavparashar486@gmail.com>
2024-01-28 20:38:56 +0530

    add something

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const plm = require("passport-local-mongoose")
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/pin")
+mongoose.connect("mongodb://127.0.0.1:27017/Pinterest_Clone")
 
 const userSchema = mongoose.Schema({
   username: {
